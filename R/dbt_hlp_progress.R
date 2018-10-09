c7e76ae0ef5db775e3d9f9d013085dc605087b29
petermeissner <retep.meissner@gmail.com>
2018-10-09 12:47:43 +0200

    fix: dbt_hlp_progress had minor issues

--- R/dbt_hlp_progress.R
+++ R/dbt_hlp_progress.R
@@ -4,6 +4,7 @@
 #' @param ii number of iterations / ... in total
 #' @param start start time
 #'
+#' @return
 #' @export
 #'
 #' @examples
@@ -11,13 +12,13 @@
 #' dbt_hlp_progress(1)
 #' dbt_hlp_progress(3, 10, Sys.time()-10)
 dbt_hlp_progress <-
-  function(i = NULL, ii = NULL, start = NULL, redraw = FALSE){
+  function(i = NULL, ii = NULL, start = NULL, redraw = FALSE, now = Sys.time()){
     if(redraw){
       cat("\r")
     }else{
       cat("\n")
     }
-    cat( as.character(Sys.time()) )
+    cat( as.character(now) )
 
     if( !is.null(i) ){
       cat(" |", i)
@@ -28,19 +29,21 @@ dbt_hlp_progress <-
     }
 
     if( !is.null(start) ){
-      cat(" | elapsed:",as.character(hms::as.hms(round(difftime(Sys.time(), start, units="sec")))))
+      cat(" | elapsed:",as.character(hms::as.hms(round(difftime(now, start, units="sec")))))
     }
 
     if( !is.null(start) & !is.null(i) & !is.null(ii)){
-      time_elapsed <- as.integer(round(difftime(Sys.time(), start, units="sec")))
-      time_eta     <- time_elapsed/i * (ii - i)
+      time_elapsed <- max(0.0001, as.integer(difftime(now, start, units="sec")))
+      time_eta     <- (time_elapsed / i) * (ii - i)
       percent_done <- time_elapsed / (time_elapsed + time_eta)
+      time_elapsed <- round(time_elapsed)
 
-      cat(" | eta:", as.character(hms::as.hms(time_elapsed)))
+      cat(" | eta:", as.character(hms::as.hms(round(time_eta))))
+
       cat(
         " |",
-        rep("=", (  round(percent_done,1))*10) ,
-        rep(".", (  round(1-percent_done,1))*10) ,
+        rep("=",   round(percent_done*10)) ,
+        rep(".",   round((1-percent_done)*10)) ,
         "|",
         sep = ""
       )
