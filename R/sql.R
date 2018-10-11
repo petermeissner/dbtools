9456f5ca73051bab7df0a502dc2e1c674afe9c94
petermeissner <retep.meissner@gmail.com>
2018-10-11 13:10:45 +0200

    sql

---
+++ R/sql.R
@@ -0,0 +1,79 @@
+#' sql
+#'
+#'
+#' @param query_string a (character) vector or SQL to be either marked as SQL or
+#'     to be interpolated.
+#' @param ... optional furtherarguments used to interpolate values into
+#'     query_string
+#'
+#' @return an object of type SQL
+#'
+#' @export
+#'
+#'
+#'
+setGeneric(
+  name = "sql",
+  def  = function(query_string, ...) standardGeneric("sql")
+)
+
+
+
+
+#' @rdname hidden_aliases
+#' @export
+setMethod(
+  f          = "sql",
+  signature  =
+    signature(
+      query_string = "ANY"
+    ),
+
+  definition =
+    function(query_string, ...) {
+      # paste together string, mark as SQL and return
+      DBI::SQL(glue::glue(query_string, ...))
+    }
+)
+
+
+
+
+
+#' @rdname hidden_aliases
+#' @export
+setMethod(
+  f          = "sql",
+  signature  =
+    signature(
+      query_string = "SQL"
+    ),
+
+  definition =
+    function(query_string, ...) {
+      # paste together string, mark as SQL and return
+      DBI::SQL(glue::glue(query_string, ...))
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
